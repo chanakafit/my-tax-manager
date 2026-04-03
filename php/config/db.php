9e83407711fa6c89cce11def756eaabffaad95eb
Chanaka Karunarathne <chanakasuranga@gmail.com>
2026-04-03 08:34:16 +0530

    updates

--- php/config/db.php
+++ php/config/db.php
@@ -1,6 +1,15 @@
 <?php
 
-$dbLocal = require(__DIR__ . '/db-local.php');
+$dbLocal = file_exists(__DIR__ . '/db-local.php')
+    ? require(__DIR__ . '/db-local.php')
+    : [
+        'host'        => getenv('DB_HOST') ?: 'localhost',
+        'port'        => getenv('DB_PORT') ?: '3306',
+        'dbname'      => getenv('DB_NAME') ?: 'mybs',
+        'username'    => getenv('DB_USER') ?: 'root',
+        'password'    => getenv('DB_PASSWD') ?: '',
+        'tablePrefix' => getenv('DB_PREFIX') ?: '',
+    ];
 
 return [
     'class' => 'yii\db\Connection',
