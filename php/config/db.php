eba8a9bc2761279e6e4bf7537197b90c1f8541ac
Chanaka Karunarathne <chanakasuranga@gmail.com>
2026-04-03 22:56:43 +0530

    DB fixes

--- php/config/db.php
+++ php/config/db.php
@@ -3,11 +3,11 @@
 $dbLocal = file_exists(__DIR__ . '/db-local.php')
     ? require(__DIR__ . '/db-local.php')
     : [
-        'host'        => getenv('DB_HOST') ?: 'localhost',
-        'port'        => getenv('DB_PORT') ?: '3306',
-        'dbname'      => getenv('DB_NAME') ?: 'mybs',
-        'username'    => getenv('DB_USER') ?: 'root',
-        'password'    => getenv('DB_PASSWD') ?: '',
+        'host'        => getenv('MYSQL_HOST') ?: 'localhost',
+        'port'        => getenv('MYSQL_PORT') ?: '3306',
+        'dbname'      => getenv('MYSQL_DATABASE') ?: 'mybs',
+        'username'    => getenv('MYSQL_USER') ?: 'root',
+        'password'    => getenv('MYSQL_PASSWORD') ?: '',
         'tablePrefix' => getenv('DB_PREFIX') ?: '',
     ];
 
