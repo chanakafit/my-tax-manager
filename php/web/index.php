3e39bd4b226fad2e84b66d27b699f138847a55d6
Chanaka Karunarathne <chanakasuranga@gmail.com>
2026-04-03 23:35:10 +0530

    Update index.php

--- php/web/index.php
+++ php/web/index.php
@@ -1,7 +1,8 @@
 <?php
 
 $appEnv = getenv('APP_ENV') ?: 'dev';
-defined('YII_DEBUG') or define('YII_DEBUG', $appEnv !== 'prod');
+$yiiDebug = getenv('YII_DEBUG');
+defined('YII_DEBUG') or define('YII_DEBUG', $yiiDebug === 'true' || ($yiiDebug !== 'false' && $appEnv !== 'prod'));
 defined('YII_ENV') or define('YII_ENV', $appEnv);
 
 require __DIR__ . '/../vendor/autoload.php';
